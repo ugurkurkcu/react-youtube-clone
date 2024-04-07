e555d1e7e72249cdb2bc4834c210394fcb6fefb8
Uğur Kürkçü <ugurkurkcu87@gmail.com>
2024-04-07 11:06:51 +0300

    env guncellendi

--- src/utils/api.js
+++ src/utils/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
     lang: "tr-TR",
   },
   headers: {
-    "X-RapidAPI-Key": "a01f8c9863msh4496ae4728b9bfbp1e00f2jsn1c873837ceb9",
+    "X-RapidAPI-Key": import.meta.env.VITE_API_KEY,
     "X-RapidAPI-Host": "yt-api.p.rapidapi.com",
   },
 });
